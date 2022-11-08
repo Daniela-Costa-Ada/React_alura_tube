7f874d2ba1c3e9c48b3070c84ca5f2c72ce9237d
Daniela-Costa-Ada <67515138+Daniela-Costa-Ada@users.noreply.github.com>
2022-11-08 13:04:42 -0300

    add first class alura react

---
+++ src/components/CSSReset.js
@@ -0,0 +1,63 @@
+import { createGlobalStyle } from "styled-components";
+
+export const CSSReset = createGlobalStyle`
+  /* Reset */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+    font-family: sans-serif;
+}
+  /* NextJS */
+html {
+    display: flex;
+    flex-direction: column;
+    min-height: 100%;
+}
+body {
+    display: flex;
+    flex: 1;
+}
+#__next {
+    display: flex;
+    flex: 1;
+}
+  /* Globals */
+button,
+a {
+    text-decoration: none;
+    opacity: 1;
+    transition: .3s;
+    &:hover,
+    &:focus {
+    opacity: .5;
+    }
+}
+* {
+  outline: 0;
+  scrollbar-color: #000 transparent;
+  scrollbar-width: thin;
+
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  &::-webkit-scrollbar {
+    width: 12px;
+    height: 12px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    border: 3px solid transparent;
+    background: #000;
+    background-clip: content-box;
+  }
+
+  &::-webkit-scrollbar-corner {
+    background-color: transparent;
+  }
+}
+`;
